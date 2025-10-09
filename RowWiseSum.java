5b4e35778460673fcb7f0c984e50040e7b363b1b
Vethika <vethika1681@gmail.com>
2025-10-09 21:20:22 +0530

    similar to columnwise sum

---
+++ RowWiseSum.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class RowWiseSum{
+    public static void main(String args[]){
+         Scanner sc = new Scanner (System.in);
+        int m =sc.nextInt();
+        int  n =sc.nextInt();
+
+        int matrix[][] = new int [m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                matrix[i][j]=sc.nextInt();
+            }
+        }
+        rowsum(matrix,m,n);
+    }
+    public static void rowsum(int [][] matrix, int m, int n){
+        for(int i=0;i<m;i++){
+            int sum=0;
+            for(int j=0;j<n;j++){
+                sum+=matrix[i][j];
+            }
+            System.out.print(sum+" ");
+        }
+    }
+}
